e905cd8917321d7f1427b1952fc7bb43fcf61014
Aman Agrawal <Aman_Agrawal@epam.com>
2023-10-25 21:25:14 +0530

    ch_3_LayingTheFoundation

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // ReactElement(object)=> HTML(browser understands)
-const parent = React.createElement(
-  "div",
-  {},
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, "Hello from h1"),
-    React.createElement("h2", {}, "Hello from h2"),
-  ])
-);
 
-// to solve complexity of writing react code from createelement we Use 
-// JSX
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "xyz" },
-  "Hello Aman from react"
-);
-console.log(parent); // object
+//JSX- HTML Like structure or XML Like structure
+//JSX- (transpiled before it reaches the JS)- Parcel- Babel
 
+//JSX=> React.createElement=> ReactElement-JS Object =>HTMLELement(Render)
+const Title = () => (
+  <h1 className="heading" tabIndex="5">
+    Hello From Namaste JSX
+  </h1>
+);
+//babel- is js compiler - convert jsx to react.createlement
+
+//functional component react
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      {Title()}
+      <Title/>
+      <Title></Title>
+      <h1>Hello Namaste from component</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //render comvert object to html tag
-root.render(parent);
+root.render(<HeadingComponent />);
