cc041ca4132de410fc3ecb34c63f9c5ee96caf07
Aman Agrawal <Aman_Agrawal@epam.com>
2023-11-21 14:37:52 +0530

    12.4 SwiggyLogoSvg added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import swiggyLogo from "../utils/swiggyLogo.svg";
 import "./ButtonCss.css";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
@@ -14,34 +15,38 @@ const Header = () => {
   const cartItems = useSelector((store) => store.cart.items);
   console.log(cartItems);
   return (
-    <div className="header flex justify-between shadow-xl h-28 items-center">
+    <div className="header flex justify-between shadow-xl h-28 items-center dark:bg-[#0F0F0F] dark:shadow-xl">
       <div className="logo-container h-full ">
-        <img
-          src={LOGO_URL}
-          alt="Logo_Image"
-          className="logo w-full h-full px-[30px]"
-        />
+        <Link to="/">
+          <img
+            src={swiggyLogo}
+            alt="Logo_Image"
+            className="logo w-full h-full px-[30px]"
+          />
+        </Link>
       </div>
       <div className="nav-items px-16">
         <ul className="flex list-none text-2xl">
-          <li className="p-2 m-2">{onlineStatus ? "Online🟢" : "Offline🔴"}</li>
-          <li className="p-2 m-2">
+          <li className="p-2 m-2 dark:text-white">
+            {onlineStatus ? "Online🟢" : "Offline🔴"}
+          </li>
+          <li className="p-2 m-2 dark:text-white">
             <Link to="/">Home</Link>
           </li>
-          <li className="p-2 m-2">
+          <li className="p-2 m-2 dark:text-white">
             <Link to="/about">About</Link>
           </li>
-          <li className="p-2 m-2">
+          <li className="p-2 m-2 dark:text-white">
             <Link to="/contact">Contact</Link>
           </li>
-          <li className="p-2 m-2">
+          <li className="p-2 m-2 dark:text-white">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li className="p-2 m-2 font-bold text-black">
+          <li className="p-2 m-2 font-bold text-black dark:text-white">
             <Link to="/cart">🛒({cartItems.length})</Link>
           </li>
           <button
-            className="login-btn"
+            className="login-btn  dark:text-white"
             onClick={() => {
               isLogin ? setIsLogin(false) : setIsLogin(true);
             }}
